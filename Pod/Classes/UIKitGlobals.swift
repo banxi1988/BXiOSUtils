1b8c37beb7379deb8345bc16a4bdd5afef254d49
banxi1988 <banxi1988@gmail.com>
2015-12-23 15:20:11 +0800

    add global dp2dp function

--- Pod/Classes/UIKitGlobals.swift
+++ Pod/Classes/UIKitGlobals.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
-let screenScale = UIScreen.mainScreen().scale
-let rootViewController = UIApplication.sharedApplication().rootViewController
+public let screenScale = UIScreen.mainScreen().scale
+public let screenWidth = UIScreen.mainScreen().bounds.width
+public let screenHeight = UIScreen.mainScreen().bounds.height
+public var designBaseWidth:CGFloat = 375 //  // Design Pointer to Devices Pointer
 
+public func dp2dp(dp:CGFloat) -> CGFloat{
+  let coefficent = screenWidth / designBaseWidth
+  return ceil(dp * coefficent)
+}
