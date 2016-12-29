8fad1470a77f2c5fa8670ac4b8389a934c217dc6
banxi1988 <banxi1988@gmail.com>
2016-12-29 21:06:01 +0800

    add random for RandomAccessCollection

--- Pod/Classes/RandomUtils.swift
+++ Pod/Classes/RandomUtils.swift
@@ -15,3 +15,15 @@ public func choice<T>(_ s:[T]) -> T{
   let index = Int(arc4random_uniform(UInt32(s.count)))
   return s[index]
 }
+
+extension RandomAccessCollection{
+  public func random() -> Iterator.Element?{
+    if isEmpty{
+      return nil
+    }
+    srandom(UInt32(clock()))
+    let offset = arc4random_uniform(numericCast(count))
+    let i = index(startIndex, offsetBy: numericCast(offset))
+    return self[i]
+  }
+}
